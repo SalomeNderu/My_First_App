5af1fd9a203ee71fa251744d1db5d1dfcc4480b1
SalomeNderu <sallienderu@gmail.com>
2023-04-03 12:37:56 +0300

    all updates

---
+++ app/src/main/java/com/example/my_app_midmorning/layout_kenya.kt
@@ -0,0 +1,11 @@
+package com.example.my_app_midmorning
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class layout_kenya : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_layout_kenya)
+    }
+}
