c2440e85e956d418ff3eea6b646501356e588d5e
SalomeNderu <sallienderu@gmail.com>
2023-04-03 11:26:29 +0300

    all updates

---
+++ app/src/main/java/com/example/my_app_midmorning/Linear_Layout_Activity.kt
@@ -0,0 +1,11 @@
+package com.example.my_app_midmorning
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Linear_Layout_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_linear_layout)
+    }
+}
---
+++ app/src/main/java/com/example/my_app_midmorning/Relative_Layout_Activity.kt
@@ -0,0 +1,11 @@
+package com.example.my_app_midmorning
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Relative_Layout_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_relative_layout)
+    }
+}
